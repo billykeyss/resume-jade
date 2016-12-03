305d96e58b842dfc0d13c126c4cae219fff4d0fb
Bill Huang <billhuang@live.ca>
2016-12-03 16:56:07 +0100

    Add more css art

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,32 @@
 $(document).ready(function() {
+  var cloudColor = ["#d7d7d7", "#e0e0e0", "#F6F3F2", "#e9e9e9", "#e4e4e4", "#bdbdbd"];
+
+  for (var i = 1; i <= 3; i++) {
+    // divRender("about", "cloud4");
+    $("#about").prepend(
+      $('<div class="cloud4" />').css({
+        position: 'absolute',
+        top: Math.floor(Math.random() * 55) + 5 + '%',
+        left: Math.floor(Math.random() * 100) + '%',
+        'background-color': cloudColor[Math.floor(Math.random() * 5)]
+      }));
+
+    $("#about").prepend(
+      $('<div class="cloud5" />').css({
+        position: 'absolute',
+        top: Math.floor(Math.random() * 55) + 5 + '%',
+        left: Math.floor(Math.random() * 100) + '%',
+        'background-color': cloudColor[Math.floor(Math.random() * 5)]
+      }));
+
+    $("#about").prepend(
+      $('<div class="cloud6" />').css({
+        position: 'absolute',
+        top: Math.floor(Math.random() * 55) + 5 + '%',
+        left: Math.floor(Math.random() * 100) + '%'
+      }));
+  }
+
   $(document).on("scroll", onScroll);
 
   //smoothscroll
@@ -24,13 +52,25 @@ $(document).ready(function() {
 
   $(function() {
     count = 0;
-    wordsArray = ["Developer", "Traveller", "Nap Enthusiast", "Homo Sapien", "Buffet Connoisseur"];
+    wordsArray = ["developer", "traveller", "nap enthusiast", "homo Sapien", "buffet connoisseur"];
     setInterval(function() {
       count++;
       $("#word").fadeOut(400, function() {
         $(this).text(wordsArray[count % wordsArray.length]).fadeIn(400);
       });
     }, 4000);
+  });
+
+  $(function() {
+    count = 0;
+    wordsArrayShirt = ["CSS", "HTML", "JS", "Node"];
+    setInterval(function() {
+      count++;
+      $("#change-text").fadeOut(400, function() {
+        $(this).text(wordsArrayShirt[count % wordsArrayShirt.length]).fadeIn(400);
+        document.getElementById("change-text").style.animationDelay = "0.5s";
+      });
+    }, 8000);
   });
 
   $(document).scroll(function() {
@@ -45,9 +85,10 @@ $(document).ready(function() {
   $(".mouse_scroll").click(function(e) {
     e.preventDefault();
     console.log('test');
-    $('html, body').stop().animate({scrollTop: $(window).height()}, 1300);
+    $('html, body').stop().animate({
+      scrollTop: $(window).height()
+    }, 1300);
   });
-
 });
 
 function onScroll(event) {
@@ -63,3 +104,18 @@ function onScroll(event) {
     }
   });
 }
+
+function clamp(num, min, max) {
+  return num <= min ? min : num >= max ? max : num;
+}
+
+// function divRender(parent, child) {
+//   $("#" + parent).prepend(
+//     $('<div class="' + child '" />').css({
+//       position: 'absolute',
+//       top: Math.floor(Math.random() * 60) + 5 + '%',
+//       left: Math.floor(Math.random() * 100) + '%',
+//       'background-color': cloudColor[Math.floor(Math.random() * 5)]
+//     }));
+//   return;
+// }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+var obj;
+fs.readFile('data.json', 'utf8', function (err, data) {
+    if (err) {
+      throw err;
+    };
+    obj = JSON.parse(data);
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { data: obj });
 });
 
 module.exports = router;
