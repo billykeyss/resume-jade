02b68d2ec78f20dccaad418dbfebea46758c4ee1
Bill Huang <billhuang@live.ca>
2018-04-28 14:07:32 -0600

    convert jade to pug:

--- app.js
+++ app.js
@@ -17,9 +17,16 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config({path: './config.env'});
 }
 
+
+/**
+ * disable X-Powered-By header, because it exposes information
+ * about the used frameworks to potential attackers.
+ */
+// app.disable('x-powered-by');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public/images', 'bb8.png')));
