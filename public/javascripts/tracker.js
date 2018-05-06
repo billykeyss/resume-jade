9a01fac0d92c32e32824a539e897dc9bc57ae993
Bill Huang <billhuang@live.ca>
2018-05-06 12:13:36 -0600

    added tracker page

--- public/javascripts/tracker.js
+++ public/javascripts/tracker.js
@@ -1,9 +1,67 @@
-var radios = document.getElementsByName('genderS');
+let inProgress = false;
+let button = $('button'),
+	spinner = '<span class="spinner"></span>';
 
-for (var i = 0, length = radios.length; i < length; i++) {
-	if (radios[i].checked) {
-		// do whatever you want with the checked radio
-		console.log(radios[i].value);
-		break;
+button.click(function() {
+	if(document.getElementById("password").value !== "password") {
+		log("wrong password");
+		return;
 	}
-}
+	let shatter = document.getElementById("shatter").checked;
+	let flower = document.getElementById("flower").checked;
+	let oil = document.getElementById("oil").checked;
+	let kief = document.getElementById("kief").checked;
+	let strain = document.getElementById("toggle-daynight").checked ? "sativa" : "indica";
+
+	let method = '';
+	let methodRadioGroup = document.getElementsByName('duration');
+	for (var i = 0, length = methodRadioGroup.length; i < length; i++) {
+		if (methodRadioGroup[i].checked) {
+			// do whatever you want with the checked radio
+			method = methodRadioGroup[i].value;
+			break;
+		}
+	}
+
+	let timestamp = Math.round((new Date()).getTime() / 1000);
+
+	if (!button.hasClass('loading')) {
+		button.toggleClass('loading');
+		inProgress = true;
+		setTimeout(function() {
+			button.html(spinner);
+		}, 600)
+	} else {
+		button.toggleClass('loading').html("Submit");
+	}
+
+	let params = {
+		"body": {
+			"tableName": "TrackingDataTable",
+			"data": {
+				"date_time": 3,
+				"strain": strain,
+				"method": method,
+				"flower": flower,
+				"shatter": shatter,
+				"oil": oil,
+				"kief": kief,
+				"type": "Weed"
+			},
+			"operation": "create"
+		}
+	};
+
+	post('https://ztap43d22k.execute-api.us-west-2.amazonaws.com/Production',
+		JSON.stringify(params),
+		function(data, status) {
+			log(data, status);
+			if (inProgress) {
+				setTimeout(function() {
+					button.toggleClass('loading').html("Submit");
+				}, 600)
+			}
+		});
+
+	log(flower, shatter, oil, kief, method, timestamp, strain);
+})
